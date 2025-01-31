37ccdf91376eaa4060ecc0846316f2bdba8dbde9
AbbassNurLDin23 <abbassnouraldeenn@gmail.com>
2025-01-31 11:19:20 +0200

    Update App.jsx Log importing

--- tic-tac-toe/07-tic-tac-toe-starting-project/src/App.jsx
+++ tic-tac-toe/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
-import Log from "./components/log";
+import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning_combinations";
 import GameOver from "./components/GameOver";
 
